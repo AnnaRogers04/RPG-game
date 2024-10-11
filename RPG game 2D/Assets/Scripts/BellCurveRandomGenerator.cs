f3473e46a915aead41b30b4c0a15edd6ec30e929
AnnaRogers04 <2315276@students.ucreative.ac.uk>
2024-10-11 15:54:58 +0100

    ,

--- RPG game 2D/Assets/Scripts/BellCurveRandomGenerator.cs
+++ RPG game 2D/Assets/Scripts/BellCurveRandomGenerator.cs
@@ -6,30 +6,25 @@ using UnityEngine;
 public class BellCurveRandomGenerator : MonoBehaviour
 {
     public int samplecount = 1000;
-
-    void Start()
+    private void Start()
     {
         for(int i = 0; i <samplecount; i++)
         {
             int randomValue = GenerateBellCurveRandom(1, 6, 2);
             Debug.Log("Generated Value" + randomValue);
-      
-
         } 
         
     }
 
-    int GenerateBellCurveRandom(int min, int max, float peak)
+    private int GenerateBellCurveRandom(int min, int max, float peak)
     {
         float randomValue = Random.Range(0f, 1f);
         float bellCurveValue = Mathf.Pow(randomValue, 2);
-
 
         float ScaledValue = Mathf.Lerp(min, max, bellCurveValue);
         int finalValue = Mathf.Clamp(Mathf.RoundToInt(ScaledValue), min, max);
 
         return finalValue;
-
     }
 
 }
