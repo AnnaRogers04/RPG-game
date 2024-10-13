158d7ef6845da0d01dee18d304beee0253224f93
AnnaRogers04 <2315276@students.ucreative.ac.uk>
2024-10-13 12:52:39 +0100

    working d6 d12 d20 dice

--- RPG game 2D/Assets/Scripts/BellCurveRandomGenerator.cs
+++ RPG game 2D/Assets/Scripts/BellCurveRandomGenerator.cs
@@ -2,29 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class BellCurveRandomGenerator : MonoBehaviour
 {
-    public int samplecount = 1000;
+    public int sampleCount = 1000;
+
     private void Start()
     {
-        for(int i = 0; i <samplecount; i++)
+        for (int i = 0; i < sampleCount; i++)
         {
-            int randomValue = GenerateBellCurveRandom(1, 6, 2);
-            Debug.Log("Generated Value" + randomValue);
+            int D6Dice = GenerateBellCurveRandom(1, 6, 2);
+            int D12Dice = GenerateBellCurveRandom(1, 12, 8);
+            int D20Dice = GenerateBellCurveRandom(1, 20, 12);
+            Debug.Log($"Generated D6: {D6Dice}, D12: {D12Dice}, D20: {D20Dice}");
         } 
-        
     }
 
     private int GenerateBellCurveRandom(int min, int max, float peak)
     {
-        float randomValue = Random.Range(0f, 1f);
-        float bellCurveValue = Mathf.Pow(randomValue, 2);
-
-        float ScaledValue = Mathf.Lerp(min, max, bellCurveValue);
-        int finalValue = Mathf.Clamp(Mathf.RoundToInt(ScaledValue), min, max);
-
+        float randomValue = Random.Range(0, 1f);
+        float bellCurveValue = Mathf.Pow(randomValue, peak);
+        float scaledValue = Mathf.Lerp(min, max, bellCurveValue);
+        int finalValue = Mathf.Clamp(Mathf.RoundToInt(scaledValue), min, max);
         return finalValue;
     }
-
 }
+
+
