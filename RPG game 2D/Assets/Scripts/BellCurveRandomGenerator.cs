aec0c647af7fbeaad96ec04c4f64fed892233fd9
AnnaRogers04 <2315276@students.ucreative.ac.uk>
2024-10-18 16:00:36 +0100

    dice working

--- RPG game 2D/Assets/Scripts/BellCurveRandomGenerator.cs
+++ RPG game 2D/Assets/Scripts/BellCurveRandomGenerator.cs
@@ -8,23 +8,38 @@ public class BellCurveRandomGenerator : MonoBehaviour
 
     private void Start()
     {
+ 
         for (int i = 0; i < sampleCount; i++)
         {
-            int D6Dice = GenerateBellCurveRandom(1, 6, 2);
-            int D12Dice = GenerateBellCurveRandom(1, 12, 8);
-            int D20Dice = GenerateBellCurveRandom(1, 20, 12);
-            Debug.Log($"Generated D6: {D6Dice}, D12: {D12Dice}, D20: {D20Dice}");
+             int D6Dice = GenerateBellCurveRandom(1, 6, 2); 
+             int D12Dice = GenerateBellCurveRandom(1, 12, 8);
+             int D20Dice = GenerateBellCurveRandom(1, 20, 12);
+             Debug.Log($"Generated D6: {D6Dice}, D12: {D12Dice}, D20: {D20Dice}");
         } 
     }
-
-    private int GenerateBellCurveRandom(int min, int max, float peak)
+    private int GenerateBellCurveRandom(float min, float max, float peak)
     {
-        float randomValue = Random.Range(0, 1f);
-        float bellCurveValue = Mathf.Pow(randomValue, peak);
-        float scaledValue = Mathf.Lerp(min, max, bellCurveValue);
-        int finalValue = Mathf.Clamp(Mathf.RoundToInt(scaledValue), min, max);
+        float randomValue = Random.Range(0f, 1f);
+        if (peak == (min + max) / 2)
+        {
+            randomValue = Mathf.Pow(randomValue, 2);
+        }
+        else
+        {
+            if (randomValue < 0.5f)
+            {
+                randomValue = Mathf.Lerp(min, peak, Mathf.Pow(randomValue * 2f, 2)); 
+            }
+            else
+            {
+                randomValue = Mathf.Lerp(peak, max, Mathf.Pow((randomValue - 0.5f) * 2f, 2));
+            }
+        }
+        int finalValue = Mathf.Clamp(Mathf.RoundToInt(randomValue), Mathf.RoundToInt(min), Mathf.RoundToInt(max));
+
         return finalValue;
     }
 }
+
 
 
